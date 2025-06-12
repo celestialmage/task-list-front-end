e3477c3ed78bc9fb0481947dd62417b55ac7bb96
Max <imax.mr@gmail.com>
2025-06-12 11:43:24 -0700

    Completed Wave 03
    Co-authored-by: Dehui <florasmile@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import TaskList from './components/TaskList.jsx';
 import './App.css';
+import { useState } from 'react';
 
 const TASKS = [
   {
@@ -15,13 +16,39 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [tasks, setTasks] = useState(TASKS);
+
+  const toggleTask = (id) => {
+    setTasks(tasks => {
+      return tasks.map(task => {
+        if (task.id === id) {
+          return {...task, isComplete: !task.isComplete};
+        }else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => {
+      return tasks.filter(task => {
+        return task.id != id;
+      });
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList
+          tasks={tasks}
+          toggleTask={toggleTask}
+          removeTask={removeTask}
+        />}</div>
       </main>
     </div>
   );
