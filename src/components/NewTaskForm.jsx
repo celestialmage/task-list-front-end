0dce97545ce49a78b8352a0f7cd0f3a384453a18
dehuihu <dehuihu@gmail.com>
2025-06-20 11:53:17 -0700

    Refactored multiple input fields into one object variable and add css to NewTaskForm

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -2,53 +2,50 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import './NewTaskForm.css';
 
+const kDefaultFormData = {
+  title: '',
+  description: '',
+};
+
 const NewTaskForm = ({ onPostTask }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
+  const [formData, setFormData] = useState(kDefaultFormData);
+  // const [title, setTitle] = useState('');
+  // const [description, setDescription] = useState('');
   const handleChange = (event) => {
-    if (event.target.id === 'task-title') {
-      setTitle(event.target.value);
-    } else {
-      setDescription(event.target.value);
-    }
+    // if (event.target.id === 'task-title') {
+    //   setTitle(event.target.value);
+    // } else {
+    //   setDescription(event.target.value);
+    // }
+    setFormData(formData => {
+      return {...formData, [event.target.name]: event.target.value };});
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newTaskData = {
-      title,
-      description,
-    };
-    onPostTask(newTaskData);
-    setTitle('');
-    setDescription('');
+    onPostTask(formData);
+    setFormData(kDefaultFormData);
+  };
+  const generateInput = (inputName) => {
+    return (
+      <div>
+        <label htmlFor={`task-${inputName}`}>Task {inputName}:</label>
+        <input
+          id={`task-${inputName}`}
+          type="text"
+          name={inputName}
+          value={formData[inputName]}
+          onChange={handleChange}
+        />
+      </div>
+    );
   };
   return (
     <section className="new-task-section">
       <h2>Add a new task</h2>
       <form className="new-task-form" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label className="form-label" htmlFor="task-title">Task title:</label>
-          <input
-            className="form-input"
-            id="task-title"
-            type="text"
-            name="title"
-            value={title}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="form-group">
-          <label className="form-label" htmlFor="task-description">Task description:</label>
-          <input
-            className="form-input"
-            id="task-description"
-            type="text"
-            name="description"
-            value={description}
-            onChange={handleChange}
-          />
-        </div>
-        <button className="form-button" type="submit">Add a new Task</button>
+        {generateInput('title')}
+        {generateInput('description')}
+        <button className="form-button" type="submit">Click to add</button>
       </form>
     </section>
   );
